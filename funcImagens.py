b9fa4b308c3edc5a1f701d79a2a75c4ec51d6e22
RacconWebFenix <dfenixweb@gmail.com>
2021-06-03 17:30:57 -0300

    Adicionado Atributo IMG para os Pokimons

---
+++ funcImagens.py
@@ -0,0 +1,107 @@
+import ascii_magic
+
+imagePathPokebola = ascii_magic.from_image_file("./img/pokebola.png")
+
+imagePathAsh = ascii_magic.from_image_file("./img/ash.png")
+
+imagePathCoracao = ascii_magic.from_image_file("./img/coracao.jpg")
+
+imagePathDuda = ascii_magic.from_image_file("./img/duda.jpeg")
+
+imagePathDerrota = ascii_magic.from_image_file("./img/derrota.png")
+
+imagePathGustavo = ascii_magic.from_image_file("./img/gustavoLima.jpeg")
+
+imagePathMisty = ascii_magic.from_image_file("./img/Misty.png")
+
+imagePathVitoria = ascii_magic.from_image_file("./img/vitoria.png")
+
+imagePathBulbasaur = ascii_magic.from_image_file("./img/bulbasaur.png")
+
+imagePathCharmander = ascii_magic.from_image_file("./img/charmander.png")
+
+imagePathChicorita = ascii_magic.from_image_file("./img/chicorita.png")
+
+imagePathEevee = ascii_magic.from_image_file("./img/eevee.png")
+
+imagePathPidgeotto = ascii_magic.from_image_file("./img/pidgeotto.png")
+
+imagePathPikachu = ascii_magic.from_image_file("./img/pikachu.png")
+
+imagePathSnorlax = ascii_magic.from_image_file("./img/snorlax.png")
+
+imagePathStarly = ascii_magic.from_image_file("./img/starly.png")
+
+imagePathStaryu = ascii_magic.from_image_file("./img/staryu.png")
+
+imagePathZubat = ascii_magic.from_image_file("./img/zubat.png")
+
+
+
+
+def imgPrintPokebola():
+    ascii_magic.to_terminal(imagePathPokebola)
+
+def imgPrintAsh():
+    ascii_magic.to_terminal(imagePathAsh)
+
+def imgPrintCoracao():
+    ascii_magic.to_terminal(imagePathCoracao)
+
+def imgPrintDuda():
+    ascii_magic.to_terminal(imagePathDuda)
+
+def imgPrintDerrota():
+    ascii_magic.to_terminal(imagePathDerrota)
+
+def imgPrintGustavo():
+    ascii_magic.to_terminal(imagePathGustavo)
+
+def imgPrintMisty():
+    ascii_magic.to_terminal(imagePathMisty)
+
+def imgPrintVitoria():
+    ascii_magic.to_terminal(imagePathVitoria)
+
+def imgPrintBulbasaur():
+    ascii_magic.to_terminal(imagePathBulbasaur)
+
+def imgPrintCharmander():
+    ascii_magic.to_terminal(imagePathCharmander)
+
+def imgPrintChicorita():
+    ascii_magic.to_terminal(imagePathChicorita)
+
+def imgPrintEevee():
+    ascii_magic.to_terminal(imagePathEevee)
+
+def imgPrintPidgeotto():
+    ascii_magic.to_terminal(imagePathPidgeotto)
+
+def imgPrintPikachu():
+    ascii_magic.to_terminal(imagePathPikachu)
+
+def imgPrintSnorlax():
+    ascii_magic.to_terminal(imagePathSnorlax)
+
+def imgPrintStarly():
+    ascii_magic.to_terminal(imagePathStarly)
+
+def imgPrintStaryu():
+    ascii_magic.to_terminal(imagePathStaryu)
+
+def imgPrintZubat():
+    ascii_magic.to_terminal(imagePathZubat)
+
+
+
+"""
+imgPrintPokebola()
+imgPrintAsh()
+imgPrintCoracao()
+imgPrintDuda()
+imgPrintDerrota()
+imgPrintGustavo()
+imgPrintMisty()
+imgPrintVitoria()
+"""
